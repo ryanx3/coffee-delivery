95847d32cffc2e462e7c202b07f81d18b31986b2
Ryan <ryangabriel_cap@hotmail.com>
2024-06-27 18:55:48 +0100

    feat: Criado e estilizado componente Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import { mixins } from "../../styles/mixins";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  max-width: 1280px;
+  margin: 0 auto;
+  padding-top: 2rem;
+`;
+
+export const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    border-radius: 6px;
+    padding: 0.625rem;
+    gap: 0.25rem;
+    background-color: ${(props) => props.theme.colors["purple-light"]};
+
+    svg {
+      fill: ${(props) => props.theme.colors.purple};
+    }
+  }
+
+  > a {
+    background-color: ${(props) => props.theme.colors["yellow-light"]};
+    padding: 0.5rem;
+    border-radius: 6px;
+    position: relative;
+
+    svg {
+      fill: ${(props) => props.theme.colors["yellow-dark"]};
+
+      &:hover {
+        
+      }
+    }
+
+    span {
+      ${mixins.fonts.textS};
+      font-weight: bold;
+
+      color: ${({ theme }) => theme.colors.white};
+      background-color: ${({ theme }) => theme.colors["yellow-dark"]};
+
+      border-radius: 1000px;
+
+      width: 1.5rem;
+      height: 1.5rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      position: absolute;
+      top: 0;
+      right: 0;
+      transform: translate(50%, -50%);
+    }
+  }
+`;
