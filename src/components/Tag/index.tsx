ae1dd86363b16a10d5d3d3ba9092159c7306f666
Ryan <ryangabriel_cap@hotmail.comn>
2024-06-28 16:12:34 +0100

    feat: Criado e estilizado componente Tag

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import { TagContainer } from "./styles";
+
+interface TagProps {
+  children: ReactNode;
+}
+
+export function Tag({ children }: TagProps) {
+  return <TagContainer>{children}</TagContainer>;
+}
