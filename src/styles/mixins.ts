5194fa25dc98d4de10082543096bab923332fcff
Ryan <ryangabriel_cap@hotmail.com>
2024-06-28 16:14:41 +0100

    refactory: Corrigindo typescript e erros de linting

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -1,89 +1,92 @@
-import { css } from 'styled-components'
+import { css } from "styled-components";
 
 export const mixins = {
+
+  
   fonts: {
     titleXL: css`
-      font-family: 'Baloo 2', sans-serif;
+      font-family: "Baloo 2", sans-serif;
       font-size: 3rem;
       line-height: 130%;
       font-weight: 800;
     `,
 
     titleL: css`
-      font-family: 'Baloo 2', sans-serif;
+      font-family: "Baloo 2", sans-serif;
       font-size: 2rem;
       line-height: 130%;
       font-weight: 800;
     `,
 
     titleM: css`
-      font-family: 'Baloo 2', sans-serif;
+      font-family: "Baloo 2", sans-serif;
       font-size: 1.5rem;
       line-height: 130%;
       font-weight: 800;
     `,
 
     titleS: css`
-      font-family: 'Baloo 2', sans-serif;
+      font-family: "Baloo 2", sans-serif;
       font-size: 1.25rem;
       line-height: 130%;
       font-weight: 700;
     `,
 
     titleXS: css`
-      font-family: 'Baloo 2', sans-serif;
+      font-family: "Baloo 2", sans-serif;
       font-size: 1.125rem;
       line-height: 130%;
       font-weight: 700;
     `,
 
     textL: css`
-      font-family: 'Roboto', sans-serif;
+      font-family: "Roboto", sans-serif;
       font-size: 1.25rem;
       line-height: 130%;
       font-weight: 400;
     `,
 
     textM: css`
-      font-family: 'Roboto', sans-serif;
+      font-family: "Roboto", sans-serif;
       font-size: 1rem;
       line-height: 130%;
       font-weight: 400;
     `,
 
     textS: css`
-      font-family: 'Roboto', sans-serif;
+      font-family: "Roboto", sans-serif;
       font-size: 0.875rem;
       line-height: 130%;
       font-weight: 400;
     `,
 
     textXS: css`
-      font-family: 'Roboto', sans-serif;
+      font-family: "Roboto", sans-serif;
       font-size: 0.75rem;
       line-height: 130%;
       font-weight: 700;
     `,
 
     tag: css`
-      font-family: 'Roboto', sans-serif;
+      font-family: "Roboto", sans-serif;
       font-size: 0.625rem;
       line-height: 130%;
-      font-weight: 700;
+      font-weight: bold;
+      color: ${(props) => props.theme.colors["yellow-dark"]};
     `,
 
     buttonG: css`
-      font-family: 'Roboto', sans-serif;
+      font-family: "Roboto", sans-serif;
       font-size: 0.875rem;
       line-height: 160%;
       font-weight: 700;
     `,
 
     buttonM: css`
-      font-family: 'Roboto', sans-serif;
+      font-family: "Roboto", sans-serif;
       font-size: 0.75rem;
       line-height: 160%;
       font-weight: 400;
     `,
   },
-}
+};
