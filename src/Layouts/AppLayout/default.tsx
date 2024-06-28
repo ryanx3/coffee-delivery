add84d4db6824ae51d09adf6a89dd42d4a832e8c
Ryan <ryangabriel_cap@hotmail.comn>
2024-06-28 16:11:45 +0100

    feat: Criado layout para definir tamanho do conteúdo

--- src/Layouts/AppLayout/default.tsx
+++ src/Layouts/AppLayout/default.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../components/Header";
+import { AppLayoutContainer } from "./styles";
 
 export function AppLayout() {
   return (
-    <div>
+    <AppLayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </AppLayoutContainer>
   );
 }
---
+++ src/Layouts/AppLayout/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const AppLayoutContainer = styled.div`
+  max-width: 1280px;
+  padding: 0 1rem;
+  margin: 0 auto;
+`
