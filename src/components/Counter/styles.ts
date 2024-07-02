62eec51b5e3a26adcf244237e9c08df2dbf930f4
Ryan <ryangabriel_cap@hotmail.comn>
2024-07-02 18:04:39 +0100

    refactory: Limpando códigos não utilizados do component Counter

--- src/components/Counter/styles.ts
+++ src/components/Counter/styles.ts
@@ -6,9 +6,12 @@ export const CounterContainer = styled.div`
   align-items: center;
   gap: 0.25rem;
   padding: 0.5rem;
+  max-width: fit-content;
 
   background-color: ${({ theme }) => theme.colors["base-button"]};
   border-radius: 6px;
+  transition: all 0.2s;
+
   span {
     ${mixins.fonts.textM}
     color: ${({ theme }) => theme.colors["base-title"]};
@@ -19,10 +22,12 @@ export const CounterContainer = styled.div`
     align-items: center;
     color: ${({ theme }) => theme.colors.purple};
     background: none;
-
     transition: all 0.2s;
+  }
 
-    &:hover {
+  &:hover {
+    background-color: ${({ theme }) => theme.colors["base-hover"]};
+    button {
       color: ${({ theme }) => theme.colors["purple-dark"]};
     }
   }
