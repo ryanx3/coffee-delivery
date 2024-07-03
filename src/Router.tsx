c3f98b90dd57e7ddb7f903aa88ba3b41010596ad
Ryan <ryangabriel_cap@hotmail.comn>
2024-07-03 23:40:18 +0100

    feat: Adicionando novas páginas a rota da aplicação

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { AppLayout } from "./Layouts/AppLayout/default";
+import { Checkout } from "./pages/Checkout";
+import { Success } from "./pages/Sucess";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout/>}>
       <Route path="/" element={<Home />} />
+      <Route path="/success" element={<Success />} />
+      <Route path="/checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
