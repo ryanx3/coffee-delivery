6c86c4cecaa63d9052216d8e42e06c7e660b6b3d
Ryan <ryangabriel_cap@hotmail.com>
2024-07-04 13:28:16 +0100

    feat: Criado TagSearch para filtrar produtos com o RadioGroup do RadixUI

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,39 @@
 import { Card } from "../../components/Card";
-import { Tag } from "../../components/Tag";
 import { HeroSection } from "./components/HeroSection";
 
-import { CardWrapper, HomeContainer, ContentSection } from "./styles";
+import { CardWrapper, HomeContainer, ContentSection, TagSearchContainer, TagSearchButton } from "./styles";
 
 export function Home() {
   return (
     <HomeContainer>
       <HeroSection />
-        <ContentSection>
-          <header>
-            <h2>Nossos cafés</h2>
-            <div>
-              <Tag>Tradicionais</Tag>
-              <Tag>Tradicionais</Tag>
-              <Tag>Tradicionais</Tag>
-            </div>
-          </header>
+      <ContentSection>
+        <header>
+          <h2>Nossos cafés</h2>
+          <TagSearchContainer>
+            <TagSearchButton value="Tradicionais" >
+              Tradicionais
+            </TagSearchButton>
+            <TagSearchButton value="Gelado">
+              Gelado
+            </TagSearchButton>
+            <TagSearchButton value="Com leite" >
+              Com leite
+            </TagSearchButton>
+          </TagSearchContainer>
+        </header>
 
-          <CardWrapper>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-          </CardWrapper>
-        </ContentSection>
+        <CardWrapper>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </CardWrapper>
+      </ContentSection>
     </HomeContainer>
   );
 }
