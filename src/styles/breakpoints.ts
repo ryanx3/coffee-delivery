95dff744f025e1c4604229fea6e50ba12ce3cd17
Ryan <ryangabriel_cap@hotmail.com>
2024-07-04 13:27:17 +0100

    feat: Criado styles de breakpoints para organizar media queries do app

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,24 @@
+
+interface Size {
+  xs: string;
+  sm: string;
+  md: string;
+  lg: string;
+  xl: string;
+}
+
+const size: Size = {
+  xs: "320px",
+  sm: "768px",
+  md: "1024px",
+  lg: "1120px",
+  xl: "1280px",
+};
+
+export const breakpoints = {
+  xs: `(max-width: ${size.xs})`,
+  sm: `(max-width: ${size.sm})`,
+  md: `(max-width: ${size.md})`,
+  lg: `(max-width: ${size.lg})`,
+  xl: `(max-width: ${size.xl})`,
+};
