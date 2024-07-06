b5a6e87a05a73212d12146e291ae69f1d4e595d9
Ryan <ryangabriel_cap@hotmail.comn>
2024-07-07 00:47:25 +0100

    feat: Instalando react-toastify para notificações do app

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,27 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/global";
 import { Router } from "./Router";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        limit={3}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <CartProvider>
         <GlobalStyle />
         <BrowserRouter>
