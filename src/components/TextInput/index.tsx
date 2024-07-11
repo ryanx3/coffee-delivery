8e3f81f823c9d2466c635033230ca4db97fb2337
Ryan <ryangabriel_cap@hotmail.com>
2024-07-11 01:03:45 +0100

    feat: Adicionando propriedades de Click para o button de pesquisa do InputText

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,17 +1,28 @@
 import { InputHTMLAttributes, forwardRef } from "react";
 import { TextInputContainer, InputWrapper } from "./styles";
+import { LiaSearchLocationSolid } from "react-icons/lia";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  optionText?: string;
+  OptionalText?: string;
+  ButtonSearch?: boolean;
+  onButtonClick?: () => void;
 }
 
 export const TextInput = forwardRef<HTMLInputElement, InputProps>(
-  ({ placeholder, optionText, ...rest }, ref) => {
+  (
+    { placeholder, OptionalText, ButtonSearch, onButtonClick, ...rest },
+    ref
+  ) => {
     return (
       <TextInputContainer>
         <InputWrapper>
           <input type="text" ref={ref} placeholder={placeholder} {...rest} />
-          {optionText && <span>{optionText}</span>}
+          {OptionalText && <span>{OptionalText}</span>}
+          {ButtonSearch && (
+            <button onClick={onButtonClick} type="button">
+              <LiaSearchLocationSolid title="Clique para pesquisar" size={26} />
+            </button>
+          )}
         </InputWrapper>
       </TextInputContainer>
     );
