ecef0c8e21cf136be5aac568df983eb1baaf2b61
Ryan <ryangabriel_cap@hotmail.com>
2024-07-17 22:34:49 +0100

    feat: Exportando todas as funções de dentro de CartContextProps em um hook para que quando eu importar algo do meu hook, o componente seja rerenderizado apenas quando a função específicada mudar

--- src/hooks/UseCart.ts
+++ src/hooks/UseCart.ts
@@ -1,7 +1,16 @@
-import { useContext } from "react";
-import { CartContext } from "../context/CartContext";
+import { useContextSelector } from "use-context-selector";
+import { CartContext, CartContextProps } from "../context/CartContext";
 
-export function useCart() {
-  const context = useContext(CartContext);
-  return context;
+export function useCart(): CartContextProps {
+  return useContextSelector(CartContext, (context) => ({
+    cartItems: context.cartItems,
+    cartTotalQuantity: context.cartTotalQuantity,
+    cartItemTotalPrice: context.cartItemTotalPrice,
+    orderData: context.orderData,
+    setOrderData: context.setOrderData,
+    AddCoffeeToCart: context.AddCoffeeToCart,
+    removeCoffeeToCart: context.removeCoffeeToCart,
+    clearCart: context.clearCart,
+    updatedQuantityOfCoffees: context.updatedQuantityOfCoffees,
+  }));
 }
